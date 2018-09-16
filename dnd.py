c0d670d2529d092dbe4279268940c2bd343a08b5
EthanJWright <ewright362@gmail.com>
2018-09-15 22:07:01 -0600

    Added history tracking

--- dnd.py
+++ dnd.py
@@ -4,6 +4,14 @@ from pyfiglet import figlet_format
 from termcolor import cprint
 import os
 import sys
+import json
+import operator
+
+if os.path.isfile('history.json'):
+    with open('history.json') as f:
+        history = json.load(f)
+else:
+    history = {}
 
 
 def printer(die_type, result):
@@ -50,6 +58,7 @@ def advantage(user):
     if ('+' in user):
         broken = user.split('+')
         adding = int(broken[-1])
+
     if ('-' in user):
         broken = user.split('-')
         adding = 0 - int(broken[-1])
@@ -73,6 +82,7 @@ def disadvantage(user):
     if ('+' in user):
         broken = user.split('+')
         adding = int(broken[-1])
+
     if ('-' in user):
         broken = user.split('-')
         adding = 0 - int(broken[-1])
@@ -100,12 +110,25 @@ def print_usage():
     print("d+3 means d20 disadvantage roll + 3 bonus")
 
 
-def roll():
-    user = input("Enter a die roll: ")
+def roll(user):
     os.system('cls' if os.name == 'nt' else 'clear')
+
+    if user[0] == 'h':
+        get_hist()
+
+        return
+
+    global history
+
+    if user in history:
+        old = history[user]
+        history[user] = old + 1
+    else:
+        history[user] = 1
 
     if "help" in user or "-h" in user:
         print_usage()
+
         return
 
     if "a" in user:
@@ -154,10 +177,41 @@ def roll():
     printer(max_possible, final)
 
 
+def get_hist():
+    global history
+    # Sort dictionary by most used items
+    history = dict(sorted(history.items(), key=operator.itemgetter(1), reverse=True))
+    iterate = 1
+
+    for key in history:
+        print(str(iterate) + " : " + key)
+        iterate += 1
+    got_input = False
+
+    while(not got_input):
+        user = input("Select One: ")
+        try:
+            val = int(user)
+            got_input = True
+        except ValueError:
+            print("Please enter a number.")
+    iterate = 1
+
+    for key in history:
+        if (iterate == val):
+            roll(key)
+        iterate += 1
+
+    return
+
+
 while True:
     try:
-        roll()
+        user = input("Enter a die roll: ")
+        roll(user.replace(" ", ""))
     except KeyboardInterrupt:
         print("\n")
         print("Exiting the program!")
+        with open('history.json', 'w') as file:
+            file.write(json.dumps(history))  # use `json.loads` to do the reverse
         sys.exit(1)
