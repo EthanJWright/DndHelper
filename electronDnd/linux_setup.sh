fa2a7744a31be51c43230029e0111626380bb1d7
EthanJWright <ewright362@gmail.com>
2018-09-17 00:41:10 -0600

    Adding installer scripts

---
+++ electronDnd/linux_setup.sh
@@ -0,0 +1,2 @@
+electron-packager ~/tmp/DndHelper/electronDnd/ DndHelper --platform=linux --arch=x64 --electronVersion=2.0.9
+
---
+++ electronDnd/mac_setup.sh
@@ -0,0 +1,2 @@
+electron-packager ~/tmp/DndHelper/electronDnd/ DndHelper --platform=darwin --arch=x64 --electronVersion=2.0.9
+
